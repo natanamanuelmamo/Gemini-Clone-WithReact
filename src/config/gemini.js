ae05ff093672534d511bc571a3549cd1d719a01d
natanamanuelmamo <natanamanuelmamo@gmail.com>
2025-02-17 15:08:16 +0300

    Final project commit

---
+++ src/config/gemini.js
@@ -0,0 +1,41 @@
+// const apiKey = "AIzaSyBxotMH3JIU_BZummKV4pu_3zbbRq7x-eg";
+// google-genai
+// npm install @google/generative-ai
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyBxotMH3JIU_BZummKV4pu_3zbbRq7x-eg";
+  // const apiKey = process.env.GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    const responseresponse = result.response.text();
+    return responseresponse;
+    // return response.text();
+  }
+  
+  export default run;
